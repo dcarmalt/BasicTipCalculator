b55164aa16562e5895f5413f770ca277101cbb81
Dianna <dianna.carmalt@getronics.com>
2019-05-07 12:40:24 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Dianna Carmalt on 5/7/19.
+//  Copyright © 2019 Dianna Carmalt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
